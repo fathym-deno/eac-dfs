de6cc79a9420522c0f32a0ccc77cb0bdd2bad85c
mcgear <michael.gearhardt@gmail.com>
2024-11-15 09:14:43 -0700

    api import tests

--- tests/test.deps.ts
+++ tests/test.deps.ts
@@ -1,3 +1,7 @@
 import "jsr:@std/dotenv@0.225.2/load";
 export { assert, assertEquals } from "jsr:@std/assert@1.0.7";
 export { delay } from "jsr:@std/async@1.0.8";
+
+export { encodeBase64 } from "jsr:@std/encoding@1.0.5/base64";
+
+export { convertFilePathToPattern } from "../src/utils/.exports.ts";
